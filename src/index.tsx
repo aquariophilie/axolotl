21910c5e02378dd7a698b99aa5389db58452858b
Raffone17 <raffoespo@gmail.com>
2021-09-10 16:23:15 +0200

    Qrcode generator for confluence with forge

---
+++ src/index.tsx
@@ -0,0 +1,89 @@
+import ForgeUI, { render, useConfig, Fragment, Image, TextField, TextArea, Select, Option, MacroConfig, Macro, Link, Text, useProductContext, useState } from "@forge/ui";
+import api, { route } from "@forge/api";
+import QRCode from "qrcode-svg";
+
+const defaultConfig = {
+  url: "https://www.atlassian.com",
+  color: "172B4D",
+  label: ""
+};
+
+const fetchPageTitleAndUrlForContent = async (contentId) => {
+  const res = await api
+    .asUser()
+    .requestConfluence(route`/wiki/rest/api/content/${contentId}`);
+
+  const data = await res.json();
+  const result = {
+    title: '',
+    url: ''
+  };
+  if (data && data._links && data._links.self) {
+    result.url = data._links.base + data._links.tinyui;
+    result.title = data.title;
+  }
+  return result;
+};
+/* 
+const getPageContext = async () => await view.getContext(); */
+
+const Config = () => {
+  const context = useProductContext();
+  const pageData = useState(async () => await fetchPageTitleAndUrlForContent(context.contentId));
+  /* console.log(JSON.stringify(pageContext)); */
+  return (
+    <MacroConfig>
+      <TextArea isRequired={true} label="URL" name="url" defaultValue={pageData[0].url} />
+      <TextField isRequired={false} label="LABEL" name="label" defaultValue="" />
+      <Select label="Color" name="color">
+        <Option label="N800 - Squid ink" value="172B4D" />
+        <Option label="B400 - Pacific bridge" value="0052CC" />
+        <Option label="R500 - Dragon's blood" value="BF2600" />
+        <Option label="Y400 - Cheezy blasters" value="FF991F" />
+        <Option label="G400 - Slime" value="00875A" />
+        <Option label="T400 - Prom dress" value="00A3BF" />
+        <Option label="P300 - Da' juice" value="6554C0" />
+      </Select>
+    </MacroConfig>
+  );
+}
+
+const App = () => {
+  const context = useProductContext();
+  const pageData = useState(async () => await fetchPageTitleAndUrlForContent(context.contentId));
+  const { url, color, label } = useConfig() || defaultConfig || { url: '', color: '172B4D', label: '' };
+  /* const { url, color } = useConfig(); */
+  const colorHex = "#" + color;
+  const qrCode = new QRCode({
+    content: url,
+    padding: 4,
+    width: 256,
+    height: 256,
+    color: colorHex,
+    background: "#ffffff",
+    ecl: "M",
+  });
+  let svg = qrCode.svg();
+  console.log(label);
+  if (label) {
+    svg = svg.replace('</svg>', '<text x="50%" y="250" fill="' + colorHex + '" text-anchor="middle">' + label + '</text>\r\n</svg>');
+  }
+
+  const encodedSvg = new Buffer(svg).toString('base64');
+  const dataUri = `data:image/svg+xml;base64,${encodedSvg}`;
+
+  return (
+    <Fragment>
+      <Image src={dataUri} alt={url} />
+      <Text>
+      </Text>
+    </Fragment>
+  );
+};
+
+export const run = render(
+  <Macro
+    app={<App />}
+  />
+);
+export const config = render(<Config />);
